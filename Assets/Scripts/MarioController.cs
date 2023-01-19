b2bb6013fab69735116bd9af6f480743d9092973
Filippo D'Addeo <filo.dadde99@gmail.com>
2023-01-19 15:58:31 +0100

    Initial commit

---
+++ Assets/Scripts/MarioController.cs
@@ -0,0 +1,74 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class AudioClips
+{
+    public AudioClip startGame;
+    public AudioClip jump;
+}
+
+[System.Serializable]
+public class Velocities
+{
+    public float forwardFactor;
+    public float horizontalFactor;
+    public float jump;
+}
+
+[RequireComponent(typeof(CharacterController))]
+public class MarioController : MonoBehaviour
+{
+    // Public variables
+    public AudioClips clips;
+    public Velocities velocities;
+
+    // Private objects for Unity Component
+    private Animator animator;
+    private CharacterController characterController;
+    private AudioSource audioSource;
+
+    // Private variable used in the code
+    private float horizontalRotation = 0.0f;
+    private float verticalVelocity = 0.0f;
+
+    private void Start()
+    {
+        animator = this.gameObject.GetComponent<Animator>();
+        characterController = this.gameObject.GetComponent<CharacterController>();
+        audioSource = this.gameObject.GetComponent<AudioSource>();
+
+        // Audio for Start Game
+        audioSource.PlayOneShot(clips.startGame);
+    }
+
+    private void Update()
+    {
+        // Retrieve speeds
+        float forwardSpeed = Input.GetAxis("Vertical") * velocities.forwardFactor;
+
+        forwardSpeed = Mathf.Max(0.0f, forwardSpeed);
+        horizontalRotation += Input.GetAxis("Horizontal") * velocities.horizontalFactor;
+        verticalVelocity += Physics.gravity.y * Time.deltaTime;
+
+        // Get space button for jump
+        if (Input.GetKeyDown(KeyCode.Space) && characterController.isGrounded)
+        {
+            verticalVelocity = velocities.jump;
+            animator.SetTrigger("Jump");
+            audioSource.PlayOneShot(clips.jump);
+        }
+
+        // Set speeds
+        Vector3 speed = new Vector3(0.0f, verticalVelocity, forwardSpeed);
+        speed = transform.rotation * speed;
+
+        // Set animations
+        animator.SetFloat("Speed", forwardSpeed);
+
+        // Move the character
+        characterController.Move(speed * Time.deltaTime);
+        characterController.transform.localRotation = Quaternion.Euler(0, horizontalRotation, 0);
+    }
+}
